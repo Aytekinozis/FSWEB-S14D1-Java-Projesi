c1e6753b030487e4925fb4a8da5c00057874b442
Aytekinozis <aytekinozis93@gmail.com>
2024-01-16 23:49:13 +0300

    fıx

--- src/com/workintech/company/HRManager.java
+++ src/com/workintech/company/HRManager.java
@@ -4,8 +4,12 @@ public class HRManager extends Employee{
     private JuniorDeveloper[] JuniorDeveloper;
     private MidDeveloper[] MidDeveloper;
     private SeniorDeveloper[] SeniorDeveloper;
-    public HRManager(int id, String name, double salary) {
+
+    public HRManager(int id, String name, double salary, com.workintech.company.JuniorDeveloper[] juniorDeveloper, com.workintech.company.MidDeveloper[] midDeveloper, com.workintech.company.SeniorDeveloper[] seniorDeveloper) {
         super(id, name, salary);
+        JuniorDeveloper = juniorDeveloper;
+        MidDeveloper = midDeveloper;
+        SeniorDeveloper = seniorDeveloper;
     }
 
     public void addEmployee(int index, JuniorDeveloper junior, MidDeveloper mid, SeniorDeveloper senior, String dev){
